cb1918042b713572bc927adb6acbbeee5c23278e
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2023-04-24 18:52:58 -0700

    commit

---
+++ server/index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const usersInRooms = {};
+let typingUsers = {};
+app.use(cors());
+app.options("*", cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+  let currentRoom;
+  let currentUser;
+
+  socket.on("join_room", (data) => {
+    const { room, username } = data;
+    currentRoom = room;
+    currentUser = username;
+    socket.join(room);
+    if (!usersInRooms[room]) {
+      usersInRooms[room] = [];
+    }
+    usersInRooms[room].push({ username, id: socket.id });
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    socket.to(room).emit("user_joined", {
+      message: `${username} has joined the room`,
+      time:
+        new Date(Date.now()).getHours() +
+        ":" +
+        new Date(Date.now()).getMinutes(),
+      author: "System",
+    });
+    socket.emit("room_users", usersInRooms[room]);
+  });
+
+  socket.on("user_typing", (data) => {
+    socket.to(data.room).emit("user_typing", data);
+  });
+
+  socket.on("user_stopped_typing", (data) => {
+    socket.to(data.room).emit("user_stopped_typing", data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  
+    for (const room in usersInRooms) {
+      const userIndex = usersInRooms[room].findIndex(
+        (user) => user.id === socket.id
+      );
+  
+      if (userIndex !== -1) {
+        const username = usersInRooms[room][userIndex].username;
+        usersInRooms[room].splice(userIndex, 1);
+  
+        socket.to(room).emit("user_left", {
+          message: `${username} has left the room`,
+          time:
+            new Date(Date.now()).getHours() +
+            ":" +
+            new Date(Date.now()).getMinutes(),
+          author: "System",
+        });
+      }
+    }
+  });
+});
+
+
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
